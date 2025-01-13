c435ae64ecb47677d5777d0aa7fa457310814b3b
Abhishek <imabhi165@gmail.com>
2025-01-13 11:10:24 +0530

    latest

---
+++ basicsOfC/functions/pANDc.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+
+
+int fact(int n){
+    int f=1;
+    for(int i=1;i<=n;i++){
+        f= f*i;
+       
+    }
+     return f;
+}
+
+
+int npr(int n,int r){
+ return fact(n) / fact(n-r);
+}
+
+int ncr(int x,int y){
+    return fact(x)/(fact(x-y)*fact(y));
+}
+
+
+
+int main() {
+   
+    int n,r,choice;
+    printf("Enter your choice : \n-------------------------------------------------------------------------\n1.Want to find NPR: \n2.Want to find NCR: \n");
+    printf("-------------------------------------------------------------------------\n");
+    scanf("%d",&choice);
+   
+    printf("\n-------------------------------------------------------------------------\n");
+    switch(choice){
+        case 1:
+         printf("Enter N : ");
+         scanf("%d",&n);
+         printf("Enter R : ");
+         scanf("%d",&r);
+         if(n>r)
+         {
+           printf("NPR is : %d",npr(n,r));
+         }
+         else 
+         {
+           printf("r should not be grater than n");
+         }
+         printf("\n-------------------------------------------------------------------------\n");
+         break;
+        case 2:
+        printf("Enter N : ");
+         scanf("%d",&n);
+         printf("Enter R : ");
+         scanf("%d",&r);
+         if(n>r)
+         {
+           printf("NPR is : %d",npr(n,r));
+         }
+         else 
+         {
+           printf("r should not be grater than n");
+         }
+         printf("NCR is : %d",ncr(n,r));
+          printf("\n-------------------------------------------------------------------------\n");
+         break;
+        default :
+         printf("You Entered Wrong Entry");
+    }
+   
+  
+}
