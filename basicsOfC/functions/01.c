c1d53be891630aa0f0ec51d33fab768270e790cb
Abhishek <imabhi165@gmail.com>
2025-01-13 09:58:19 +0530

    changes

--- basicsOfC/functions/01.c
+++ basicsOfC/functions/01.c
@@ -1,16 +1,16 @@
 
 
 #include<stdio.h>
-int add(int a, int b){
+int add(int a, int b){ 
     return a + b; //here a and b is paas by reference ;
 }
 int main() {
     int x,y;
     printf("Enter First Number : ");
-    scanf("%d",&x);
+    scanf("%d",&x); //here &x is the address of int type x
     printf("Enter Second Number : ");
     scanf("%d",&y);
-    printf("Sum is : %d\n",add(x,y)); //here add(x,y) is function call where x and y is int type and pass by value
+    printf("Sum is : %d\n",add(x,y));  //here add(x,y) is function call where x and y is int type and pass by value
 
 }
 
