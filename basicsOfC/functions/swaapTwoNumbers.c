8b37c3a650bb94332407a5c20586efbe198c9be5
Abhishek <imabhi165@gmail.com>
2025-01-16 09:20:20 +0530

    Anothermethod

--- basicsOfC/functions/swaapTwoNumbers.c
+++ basicsOfC/functions/swaapTwoNumbers.c
@@ -1,19 +1,41 @@
 #include<stdio.h>
-int swap(int *a,int *b){
+/*
+void swap(int *a, int *b){
     int temp;
-    temp=*a;
-    *a=*b;
-    *b=temp;        
+    temp = *a;
+    *a = *b;
+    *b = temp;
+    return;
+    
 }
-int main(){
-    int a,b;
-    printf("Enter two numbers:\n");
-    scanf("%d %d",&a,&b);
-    a=a+b;
-    b=a-b;
-    a=a-b;
-    printf("a=%d b=%d",a,b);
-    swap(&a,&b);   
-    printf("\na=%d b=%d",a,b);
-    return 0;
+*/
+
+void swap(int *x, int *y){
+    *x = *x + *y;
+    *y = *x - *y;
+    *x = *x - *y;
+    return;
+}
+
+int main() {
+int n1,n2;
+printf("Enter Two Numbers : \n");
+scanf("%d%d",&n1,&n2);
+
+printf("Before Swapping the Value of a = %d and b = %d\n",n1,n2);
+
+swap(&n1,&n2);
+
+printf("After Swapping the Value of a = %d and b = %d\n",n1,n2);
+
+
+
+
+
+
+
+
+
+
+return 0;
 }
