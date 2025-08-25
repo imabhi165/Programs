76a235c1e64e8c12bbb12ca21541cccb40d140eb
Abhishek <abhishek.k@avench.com>
2025-08-25 16:43:24 +0530

    Adding .gitignore file

--- C++_Tutorial/basics/variable.cpp
+++ C++_Tutorial/basics/variable.cpp
@@ -3,6 +3,10 @@
 using namespace std;
 int main()
 {
-
+    int a, b;
+    cout << "Enter two number: " << endl;
+    cin >> a >> b;
+    int sum = a + b;
+    cout << "Sum is : " << sum << endl;
     return 0;
 }
