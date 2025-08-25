a7d32c8dfd0bd1d770742dd67497362c468178c6
Abhishek <abhishek.k@avench.com>
2025-08-25 17:28:27 +0530

    Added python files

---
+++ Python-Tutorial/input_output.py
@@ -0,0 +1,13 @@
+name = input("Enter your name: ")
+print("Hello", name)
+
+
+
+rollNo = input("Please Enter you roll Number : ")
+print("Your Roll Number is: ",rollNo)
+
+
+a = input("Please Input Number first : ")
+b = input("Please Input Number second : ")
+sum =  (a+b)
+print("Sum is : ",sum)
--- Python-Tutorial/list.py
+++
@@ -1,4 +0,0 @@
-num = [1,2,3,4,5]
-
-for i in num:
-    print(num)
