fe1d8cd315215c4c40194f6fbc7477feeaba62ba
Abhishek <abhishek.k@avench.com>
2025-08-26 16:56:53 +0530

    Adding Oops concepts

---
+++ C++_Tutorial/Oops/Contructor/Contructor.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class MyClass
+{       // The class
+public: // Access specifier
+    MyClass()
+    { // Constructor
+        cout << "Hello World!\n";
+    }
+};
+
+int main()
+{
+    MyClass myObj; // Create an object of MyClass (this will call the constructor)
+    return 0;
+}
