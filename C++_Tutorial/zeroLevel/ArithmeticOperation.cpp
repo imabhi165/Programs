e58d9c9c5b9ceab166a84ce0a3240a292ffe6ede
imabhi165 <abhi7808060981@gmail.com>
2025-08-28 05:40:07 +0530

    Adding updated .gitignore files

--- C++_Tutorial/zeroLevel/ArithmeticOperation.cpp
+++ C++_Tutorial/zeroLevel/ArithmeticOperation.cpp
@@ -1,22 +1,20 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main() {
-// float a = 100;
-// float b = 20;
-// float sum = a + b, diff = a - b , product = a * b , div = a/b;
-// cout<<"Sum is : "<<sum<<endl;
-// cout<<"Difference is : "<<diff<<endl;
-// cout<<"Product is : "<<product<<endl;
-// cout<<"Division is : "<<div<<endl;
+int main()
+{
+    // float a = 100;
+    // float b = 20;
+    // float sum = a + b, diff = a - b , product = a * b , div = a/b;
+    // cout<<"Sum is : "<<sum<<endl;
+    // cout<<"Difference is : "<<diff<<endl;
+    // cout<<"Product is : "<<product<<endl;
+    // cout<<"Division is : "<<div<<endl;
 
-int a = 5;
-// cout<<a++<<endl;
-// cout<<++a<<endl;
-cout<<a--<<endl;
-cout<<--a<<endl;
+    int a = 5;
+    // cout<<a++<<endl;
+    // cout<<++a<<endl;
+    cout << a-- << endl;
+    cout << --a << endl;
 
-
-
-
- return 0;
+    return 0;
 }
