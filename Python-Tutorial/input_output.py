e6a24b9d047f058d93c782313e71b57dc3e9fb15
imabhi165 <abhi7808060981@gmail.com>
2025-08-29 13:51:29 +0530

    changing Python file

--- Python-Tutorial/input_output.py
+++ Python-Tutorial/input_output.py
@@ -11,3 +11,16 @@ a = input("Please Input Number first : ")
 b = input("Please Input Number second : ")
 sum =  (a+b)
 print("Sum is : ",sum)
+
+
+# a = input("Enter the First Number : ")
+# b = input("Enter the Second Number : ")
+# sum = a + b
+# print("Sum is : ",sum)
+
+#here is the code to multiply two number
+
+x = input("Enter the First Number : ")
+y = input("Enter the Second Number : ")
+mul = x * y
+print("Multiplication is : ",mul)
