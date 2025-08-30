f2103e92a6fe36c281e53b289ea4081b58049150
imabhi165 <abhi7808060981@gmail.com>
2025-08-30 09:27:45 +0530

    Added Two sum .

--- C++_Tutorial/DSA_in_C++/Twosum.cpp
+++ C++_Tutorial/DSA_in_C++/Twosum.cpp
@@ -43,11 +43,13 @@ vector<int> twoSum(vector<int>& nums, int target) {
 
 bool twosum(int arr[], int target)
 {
-    for (int i = 0; i < arr.size(); i++)
+    for (int i = 0; i < arr.length(); i++)
     {
         if (arr[i] + arr[i + 1] == target)
         {
             return true;
+
+            // i will add it later , rest solution will be added later
         }
         else
         {
