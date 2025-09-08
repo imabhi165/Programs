c622e13a76bc2bcb9171660f0ca09c4fddfdf6fd
Abhishek <abhishek.k@avench.com>
2025-09-08 09:36:46 +0530

    Added Encapsulation code

--- C++_Tutorial/Oops/Encapsulation/basic.cpp
+++ C++_Tutorial/Oops/Encapsulation/basic.cpp
@@ -1,27 +1,25 @@
 #include <iostream>
-
 using namespace std;
 
-class Employee
-{
-private:
+class Employee {
+  private:
+    // Private attribute
     int salary;
 
-public:
-    void setSalary(int s)
-    {
-        salary = s;
+  public:
+    // Setter
+    void setSalary(int s) {
+      salary = s;
     }
-    int getSalary()
-    {
-        return salary;
+    // Getter
+    int getSalary() {
+      return salary;
     }
 };
 
-int main()
-{
-    Employee myobj;
-    myobj.setSalary(50000);
-    cout << myobj.getSalary << endl;
-    return 0;
+int main() {
+  Employee myObj;
+  myObj.setSalary(50000);
+  cout << myObj.getSalary();
+  return 0;
 }
